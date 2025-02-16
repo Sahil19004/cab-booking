5c9c2de64122b20144208afcbef0b36adefcf58d
Sahil Kumar <165409654+Sahil19004@users.noreply.github.com>
2025-02-16 11:33:27 +0530

    Delete myapp directory

--- myapp/forms.py
+++
@@ -1,8 +0,0 @@
-# forms.py
-from django import forms
-from .models import CarBooking
-
-class CarBookingForm(forms.ModelForm):
-    class Meta:
-        model = CarBooking
-        fields = ['name', 'mobile', 'email', 'pickup', 'trip', 'departure', 'arrival']
